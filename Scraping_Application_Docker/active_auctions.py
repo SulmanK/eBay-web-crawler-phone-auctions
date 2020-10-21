72183d4bb0ac8532312f82f4758830ff192ccc75
Sulman Khan <43801845+SulmanK@users.noreply.github.com>
2020-10-21 13:00:58 -0400

    Update active_auctions.py

--- Scraping_Application_Docker/active_auctions.py
+++ Scraping_Application_Docker/active_auctions.py
@@ -7,35 +7,36 @@ import pandas as pd
 import psycopg2
 import requests
 # --------------------- Active Auctions
-# Pull the data from the database
-# Set up the connection
-DATABASE_URL = "enter"
-engine = create_engine(DATABASE_URL)
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-cursor = conn.cursor()
-
-# Store the data into their respective dataframes
-df_ip11 = pd.read_sql('select * from iphone_11', con=conn,)
-df_ipse = pd.read_sql('select * from apple_iphone_se', con=conn)
-df_sg10 = pd.read_sql('select * from samsung_galaxy_note_10', con=conn)
-df_sg20 = pd.read_sql('select * from samsung_galaxy_s20_ultra', con=conn)
-
 # Check if a variable is in the list
-
-
 def check_active(x, list_1):
+    """check if a variable is in the list"""
     if x in list_1:
-        x = 'YES'
-    else:
         x = 'NO'
+    else:
+        x = 'YES'
     return x
 
 # Labels auctions that are active or inactive
-
-
-def active_auctions_ids(df, phone_model):
+def active_auctions_ids(phone_model):
     "Function to retrieve active auctions"
     inactive_url = []
+
+    # Pull the data from the database
+    # Set up the connection
+    DATABASE_URL = "enter"
+    engine = create_engine(DATABASE_URL)
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cursor = conn.cursor()
+    
+    # Perform string manipulations to phone model name
+    s = phone_model
+    s = s.lower()
+    s = s.replace(' ', '_')
+
+    # Store the data into their respective dataframes
+    df = pd.read_sql('select * from ' + str(s) , con=conn, index_col = 'index')
+    df.index = [x for x in range (0, len(df))]
+    
     # Iterate through all the urls in the dataframe
     for x in df[df['active_auction'] == 'YES']['url']:
         try:
@@ -61,26 +62,21 @@ def active_auctions_ids(df, phone_model):
     # Change all entries if they are active or inactive
     df['active_auction'] = df['url'].apply(
         lambda x: check_active(x=x, list_1=inactive_url))
-
-    # PostgreSQL database
-
-    # Perform string manipulations to phone model name
-    s = phone_model
-    s = s.lower()
-    s = s.replace(' ', '_')
-
+    
+    
     # Drop original table from database
     cursor.execute('DROP TABLE IF EXISTS' + ' ' + str(s))
     conn.commit()
-
+    
     # Insert df to database
     df.to_sql(str(s), con=engine, if_exists='append')
     conn.commit()
-
+    conn.close()
+    
     return print("The data entries have been added for " + str(s))
-
+    
 # Instantiate Functions
-active_auctions_ids(df = df_ip11, phone_model = 'iPhone 11')
-active_auctions_ids(df = df_ipse, phone_model = 'Apple iPhone SE')
-active_auctions_ids(df = df_sg10, phone_model = 'Samsung Galaxy Note 10')
-active_auctions_ids(df = df_sg20, phone_model = 'Samsung Galaxy S20 Ultra')
+active_auctions_ids(phone_model = 'iPhone 11')
+active_auctions_ids(phone_model = 'Apple iPhone SE')
+active_auctions_ids(phone_model = 'Samsung Galaxy Note 10')
+active_auctions_ids(phone_model = 'Samsung Galaxy S20 Ultra')
